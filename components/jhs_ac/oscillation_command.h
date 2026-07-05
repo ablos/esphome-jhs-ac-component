a8e4a20d20cf0154453303f0a1de664b6e9adb5a
Abel Dieterich <dieterich.abel@gmail.com>
2026-07-05 18:10:01 +0200

    Swing fix

--- components/jhs_ac/oscillation_command.h
+++ components/jhs_ac/oscillation_command.h
@@ -10,7 +10,7 @@ public:
     {
         serialize_command(packet, 
             static_cast<uint8_t>(Function::Oscillation), 
-            static_cast<uint8_t>(m_status ? 0x01 : 0x00));
+            static_cast<uint8_t>(m_status ? 0x02 : 0x00));
     }
 
     void set_status(bool status) { m_status = status; }
